cff2b5bde351b3f1aeb7d5f8b8eb9a8eaa062208
timofey.verzunov <timofey.verzunov@axenix.pro>
2024-10-27 13:05:19 +0300

    update task2

--- src/main/java/lab/mifi/lab2/User.java
+++ src/main/java/lab/mifi/lab2/User.java
@@ -9,7 +9,7 @@ import lombok.ToString;
 @ToString
 public class User {
     private static int count = 0;
-    private int id = -1;
+    private int id;
     private final String firstName;
     private final String lastName;
     private final int age;
